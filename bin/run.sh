9473204b9f639f95612c1d0d38113fb184acc4c1
Nicolas <nicolas@thibault-desktop.(none)>
2012-07-05 01:28:10 +0200

    Another test

--- bin/run.sh
+++ bin/run.sh
@@ -5,24 +5,25 @@ cd `dirname $0`
 
 #Was this script started in the bin folder? if yes move out
 if [ -d "../bin" ]; then
-  cd "../"
+  cp -v "./mongodb_db.js" "./node_modules/ueberDB/mongodb_db.js"
+else
+  cd "bin"
+  cp -v "./mongodb_db.js" "./node_modules/ueberDB/mongodb_db.js"
 fi
 
-echo "Ensure that all dependencies are up to date..."
-(
-  mkdir -p node_modules
-  cd node_modules
-  [ -e ep_etherpad-lite ] || ln -s ../src ep_etherpad-lite
-  cd ep_etherpad-lite
-  npm install
-) || {
-  rm -rf node_modules
-  exit 1
-}
-
-cd "bin"
-cp -v "./mongodb_db.js" "../src/node_modules/ueberDB/mongodb_db.js"
 cd "../"
+
+#echo "Ensure that all dependencies are up to date..."
+#(
+#  mkdir -p node_modules
+#  cd node_modules
+#  [ -e ep_etherpad-lite ] || ln -s ../src ep_etherpad-lite
+#  cd ep_etherpad-lite
+#  npm install
+#) || {
+#  rm -rf node_modules
+#  exit 1
+#}
 
 #Move to the node folder and start
 echo "start..."
